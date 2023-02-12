1fd76cd519dc8c7fcddd8dbed053c191b57a161c
Thomas Calle <thos.calle@gmail.com>
2023-02-12 01:17:24 -0500

    update JS and file sort

--- Develop/index.js
+++ Develop/index.js
@@ -1,81 +1,81 @@
-// Include required packages
+// Included packages required for this file.
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generatorMarkdown = require("./utils/generateMarkdown");
+const generatorMarkdown = require("./utils/generateMarkdown.js");
 
-// Array of questions for user input
+// Array of questions for user input.
 const questions = [
     {
         type: "input",
-        name: "title",
-        message: "What is the title of your project?",
+        name: "Project Title",
+        message: "What is the name of your Project Title?",
     },
     {
         type: "input",
-        name: "description",
-        message: "Please provide a description of your project:",
+        name: "Description",
+        message: "Please, provide a short description explaining the W5H of your project:",
     },
     {
         type: "input",
-        name: "installation",
-        message: "What are the steps required to install your project?",
+        name: "Installation Instructions",
+        message: "What are the steps required to install your project? Provide a step-by-step description.",
     },
     {
         type: "input",
-        name: "usage",
-        message: "Please provide examples on how to use your project:",
+        name: "Usage Information",
+        message: "Provide instructions and examples for use. Include screenshots as needed. To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax: ```md ![alt text](assets/images/screenshot.png)```",
     },
     {
         type: "input",
-        name: "contributing",
+        name: "Contribution Guidelines",
         message: "What are the guidelines for contributing to this project?",
     },
     {
         type: "input",
-        name: "tests",
+        name: "Test Instructions",
         message: "What are the steps to run the tests for this project?",
     },
     {
         type: "list",
-        name: "license",
+        name: "License",
         message: "What license would you like to use for this project?",
         choices: ["MIT", "Apache 2.0", "GPL v3", "BSD 3", "None"],
     },
     {
         type: "input",
-        name: "github",
+        name: "GitHub",
         message: "What is your Github username?",
     },
     {
         type: "input",
-        name: "email",
+        name: "Email",
         message: "What is your email address?",
     },
 ];
 
-// Function to write README file
+// Function to write the README file.
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, function (err) {
         if (err) {
             return console.log(err);
         }
-        console.log("README file successfully created!");
+        console.log("The README.md file has beeen successfully created!");
     });
 }
 
-// Function to initialize the app
+// Function to initialize the app.
 async function init() {
-    console.log("Starting the README generator...");
+    console.log("Starting the README.md generator...");
 
-    // Prompt the user for answers
+    // Prompt the user for answers.
     const answers = await inquirer.prompt(questions);
 
-    // Generate the README content
+    // Generate the README content.
     let readmeContent = generatorMarkdown(answers);
 
-    // Write the README file
-    writeToFile("README.md", readmeContent);
+        // Write the README file.
+        writeToFile("README.md", readmeContent);
 }
 
-// Call the init function to start the app
+// Call the init function to start the app.
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  
+
 
 
 }
